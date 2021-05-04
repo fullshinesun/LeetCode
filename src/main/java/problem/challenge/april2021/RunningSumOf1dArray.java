59d278ca3938e89ad58711dc5322b06a5654364d
Misun Kim <fullshinesun@gmail.com>
2021-05-04 13:43:13 +0900

    2021-05-04

---
+++ src/main/java/problem/challenge/april2021/RunningSumOf1dArray.java
@@ -0,0 +1,19 @@
+package problem.challenge.april2021;
+
+public class RunningSumOf1dArray {
+    public int[] runningSum(int[] nums) {
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i];
+        }
+        return sum;
+    }
+
+    public int[] runningSumLessMemory(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
